73f4fcbaac40ace8d97bd4762f36fd856f267df4
anagamin <anagamin@yahoo-corp.jp>
2022-01-20 10:35:05 +0900

    fix前

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-// $('header').fadeOut('slow');
+$('a[href^="#"]').on('click', function () {
+  let time = 500;
+  let target = $(this.hash);
+  // const naviHeight = $('.Navi').outerHeight();
+  const naviHeight = '0';
+  const distance = target.offset().top - naviHeight;
+  $('html,body').animate({ scrollTop: distance }, time, 'swing');
+  console.log('hoge');
+  return false;
+});
+
+$('.js-navi').on('click', function (e) {
+  e.preventDefault();
+  $('.Tsp').toggleClass('is-show');
+});
+
+var slideUp = {
+  distance: '5%',
+  origin: 'bottom',
+  delay: 100,
+  opacity: 0,
+};
+
+ScrollReveal().reveal('.reveal', slideUp);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BeautifyHtmlWebpackPlugin = require('beautify-html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env, args) => {
@@ -82,6 +83,7 @@ module.exports = (env, args) => {
       devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         open: true, //起動時にブラウザを開く
+        overlay: true, //エラーをオーバーレイ表示
       },
 
       plugins: [
@@ -97,8 +99,36 @@ module.exports = (env, args) => {
           filename: './css/style.css',
         }),
         new HtmlWebpackPlugin({
+          inject: false,
           filename: 'index.html',
           template: './index.html',
+        }),
+        new BeautifyHtmlWebpackPlugin({
+          indent_size: 2,
+          indent_char: ' ',
+          indent_with_tabs: false,
+          editorconfig: false,
+          eol: '\n',
+          end_with_newline: false,
+          indent_level: 0,
+          preserve_newlines: true,
+          max_preserve_newlines: 2,
+          space_in_paren: false,
+          space_in_empty_paren: false,
+          jslint_happy: false,
+          space_after_anon_function: false,
+          space_after_named_function: false,
+          brace_style: 'collapse',
+          unindent_chained_methods: false,
+          break_chained_methods: false,
+          keep_array_indentation: false,
+          unescape_strings: false,
+          wrap_line_length: 0,
+          e4x: false,
+          comma_first: false,
+          operator_position: 'before-newline',
+          indent_empty_lines: false,
+          templating: ['auto'],
         }),
         new CopyPlugin({
           patterns: [
